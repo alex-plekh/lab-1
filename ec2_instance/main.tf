e9de141bbe68091e26484d419a4ad1ff93bab1c7
alona_honcharenko <alona.hon@rozdoum.com>
2024-06-05 22:22:02 +0300

    ccc

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -9,9 +9,6 @@ terraform {
 
 provider "aws" {
   region                  = "us-east-1"
-  shared_config_files     = ["~/.aws/config"]
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                 = "default"
 }
 
 # Get the latest Amazon Linux AMI ID
