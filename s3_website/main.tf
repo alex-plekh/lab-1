fdadc006e00e118ce7a59807280feb06328bcca7
alona_honcharenko <alona.hon@rozdoum.com>
2024-06-05 21:07:49 +0300

    ccc

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -19,10 +19,18 @@ resource "aws_s3_bucket" "website" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example_acl" {
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.website.id
+  rule {
+    object_ownership = "ObjectWriter"
+  }
+}
+
+resource "aws_s3_bucket_acl" "s3_bucket_acl" {
   bucket = aws_s3_bucket.website.id
   acl    = "public-read"
 }
+
 
 resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.website.id
